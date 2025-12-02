1cd9abcce3359a116306497ca297a0ed93ab3ada
sokcheadalinsorn <sokcheadalinsorn@gmail.com>
2025-12-02 16:43:07 +0700

    properety  nv kvas gallery

--- pages/property listign details/index.js
+++ pages/property listign details/index.js
@@ -1,23 +1,2 @@
-const sectionPropertiesElement = document.getElementById("similar__properties");
-
-fetch("../../public/data/properties.json")
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.length > 0) {
-      for (let i = 0; i < 6; i++) {
-        sectionPropertiesElement.innerHTML += `
-        <a class="card" href="/pages/property/${data[i].title.km}">
-            <img class="card__media" src="home-hero.png"
-                alt="home-similar" />
-            <div class="card__body">
-                <h3 class="card__title">${data[i].title.km}</h3>
-                <div class="card__meta">${data[i].price} ${data[i].currency} / month</div>
-                <span class="badge">Available</span>
-            </div>
-        </a>
-      `;
-      }
-    }
-  });
 
 
